d6a9b4e209309757d992d0988fcf98091ab0cbb9
Eric Mountain <eric.mountain@gmail.com>
2019-12-22 14:27:59 +0100

    Adds loops and lists

--- 001_basic.py
+++ 001_basic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 print("a string (of characters)")
 print(345)
@@ -23,3 +23,15 @@ print(f"a+b = {a+b}, is of type {type(a+b)}")
 e="world"
 print(f"c+e = {c+e}, is of type {type(e)}")
 
+# Lists
+a = []
+print(f"a = {a}")
+a.append("something")
+a.extend([1, 2, "bob", 10])
+print(f"after append and extend, a = {a}")
+print(f"2nd element of a = {a[1]}")
+print(f"length of a = {len(a)}")
+
+# Loops
+for i in range(3):
+    print(f"i = {i}")
